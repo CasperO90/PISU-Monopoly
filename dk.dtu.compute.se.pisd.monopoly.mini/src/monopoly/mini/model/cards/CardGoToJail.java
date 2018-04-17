ec2f42d5e0c31e915968df5ffecd5848a5c0f814
Andreas Peter Jørgensen <andreasjsen@live.dk>
2018-04-17 10:52:55 +0200

    5th commit CardGoToJail implementation

--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/cards/CardGoToJail.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/cards/CardGoToJail.java
@@ -1,7 +1,14 @@
 package monopoly.mini.model.cards;
 
+import monopoly.mini.GameController;
 import monopoly.mini.model.Card;
+import monopoly.mini.model.Player;
+import monopoly.mini.model.exceptions.PlayerBrokeException;
 
 public class CardGoToJail extends Card{
-
+	
+	public void doAction(GameController controller, Player player) throws PlayerBrokeException {
+		controller.gotoJail(player);
+		controller.returnChanceCardToDeck(this);
+	};	
 }
