7060f38775246e40a03f1313a173969d3ba5fcf3
Casper <Casper@DESKTOP-2IV7N3D.eduroam.wireless.dtu.dk>
2018-04-17 13:51:38 +0200

    9.5th commit

--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/cards/CardPayMoneyToBank.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/cards/CardPayMoneyToBank.java
@@ -1,7 +1,30 @@
 package monopoly.mini.model.cards;
 
+import monopoly.mini.GameController;
 import monopoly.mini.model.Card;
+import monopoly.mini.model.Player;
+import monopoly.mini.model.exceptions.PlayerBrokeException;
 
 public class CardPayMoneyToBank extends Card {
+	
+	private int amount;
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	public void doAction(GameController controller, Player player) throws PlayerBrokeException {
+		try {
+			controller.paymentToBank(player, amount);
+		} finally {
+			// Make sure that the card is returned to the deck even when
+			// an Exception should occur!
+			super.doAction(controller, player);
+		}
+	}
 
 }
