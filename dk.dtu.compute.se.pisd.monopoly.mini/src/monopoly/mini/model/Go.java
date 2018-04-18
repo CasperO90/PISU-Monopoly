302481802e45383f225698a74be976fb4d813094
Casper <Casper@DESKTOP-2IV7N3D.eduroam.wireless.dtu.dk>
2018-04-18 13:01:08 +0200

    11th commit

--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/Go.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/Go.java
@@ -1,5 +1,25 @@
 package monopoly.mini.model;
 
+import monopoly.mini.GameController;
+import monopoly.mini.model.exceptions.PlayerBrokeException;
+
 public class Go extends Space{
+	
+	/*
+	private int goAmount;
+	
+	
+	public int getGoAmount() {
+		return goAmount;
+	}
+	public void setGoAmount(int goAmount) {
+		this.goAmount = goAmount;
+	}
+	
+	public void doAction(GameController controller, Player player) throws PlayerBrokeException {
+		controller.paymentFromBank(player, goAmount);
+	}
+	*/
+	
 
 }
--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/GoToJail.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/GoToJail.java
@@ -1,5 +1,14 @@
 package monopoly.mini.model;
 
+import monopoly.mini.GameController;
+import monopoly.mini.model.exceptions.PlayerBrokeException;
+
 public class GoToJail extends Space {
+	
+	
+	
+	public void doAction(GameController controller, Player player) throws PlayerBrokeException {
+		controller.gotoJail(player);
+	}
 
 }
