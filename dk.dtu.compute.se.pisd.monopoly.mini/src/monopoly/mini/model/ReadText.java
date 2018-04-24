2a1923176e16a9d8fba45a6738b10791bd8348ab
Elisa <Elisa@elisa>
2018-04-24 11:17:50 +0200

    få den til at læse txt files

--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/ReadText.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/model/ReadText.java
@@ -1,4 +1,37 @@
-package monopoly.mini.model
-public class ReadText {
+package monopoly.mini.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadText {
+	public static String[] file(String fileName) {
+
+		ArrayList<String> lines1 = new ArrayList<String>();
+
+		try {
+			BufferedReader reader1 = new BufferedReader(new FileReader(fileName));
+			String line = "\r\n";
+			while ((line = reader1.readLine()) != null) {
+				lines1.add(line);
+			}
+		//System.out.println(lines1);
+		reader1.close();
+	} catch (IOException e) {
+		System.out.println("error");
+	} finally {
+	//System.out.println(fileName + " read");
+	}
+	String[] li1 = new String[lines1.size()];
+	li1 = lines1.toArray(li1);
+
+	return li1;
+	
+	}
 }
+	
+		
+
+
+
