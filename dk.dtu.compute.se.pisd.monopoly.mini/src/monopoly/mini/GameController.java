effa185f7a52df4ab9114772ecc6877bd392dbca
Casper <Casper@DESKTOP-2IV7N3D>
2018-05-02 14:40:45 +0200

    Små ændringer

--- dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/GameController.java
+++ dk.dtu.compute.se.pisd.monopoly.mini/src/monopoly/mini/GameController.java
@@ -82,7 +82,7 @@ public class GameController {
 	 * is hard-coded. But this should be done by interacting with 
 	 * the user.
 	 */
-	//@Henrik
+	//@Henrik, @Elisa, @Casper
 	public void createPlayers() {
 
 
